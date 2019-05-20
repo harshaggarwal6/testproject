baeef6ec63cea4143c59d51398bdbb12a52694a9
harsh.kumar1 <harsh.kumar1@mattsenkumar.com>
2019-05-20 17:58:04 +0530

    second commit

--- demo-1/src/main/java/com/example/demo/Demo1Application.java
+++ demo-1/src/main/java/com/example/demo/Demo1Application.java
@@ -3,11 +3,17 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author harsh.kumar1
+ *
+ */
 @SpringBootApplication
 public class Demo1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
+		System.out.println("starting the appppp....");
 	}
 
 }
